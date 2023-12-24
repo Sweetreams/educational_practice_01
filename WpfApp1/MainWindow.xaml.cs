8a111afe8bbe78aedb53f8736784e084de3227f4
Sweetreams <pashasurov12345@gmail.com>
2023-12-24 15:39:02 +0700

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using WpfApp1.Properties;
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> strings = new List<Product>();
+        List<String> products = new List<String>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Product product = new Product(Name_Product.Text, Name_Dic.Text, Convert.ToInt32(Price.Text), Shelf_Life_Pro.Text, Convert.ToInt32(Count_Product.Text));
+                products.Add($"{product.Name_Product}, {product.Name_Dic}, {product.Price_Product}, {product.Shelf_Life}, {product.Count_Product}");
+                strings.Add(product);
+            }
+            catch
+            {
+                MessageBox.Show("Неверный формат ввода");
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Lable_text.Items.Clear();
+            foreach(string s in products)
+            {
+                Lable_text.Items.Add(s);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            Lable_text.Items.Clear();
+            try
+            {
+                foreach(Product product in strings)
+                {
+                    if(product.Name_Product == Name_search.Text)
+                    {
+                        Lable_text.Items.Add(product.ToString());
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Я ФУТБОЛЬНЫЙ МЯЧИК");
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            Lable_text.Items.Clear();
+            try
+            {
+                foreach (Product product in strings)
+                {
+                    if (product.Price_Product < Convert.ToInt32(Name_price_s.Text))
+                    {
+                        Lable_text.Items.Add(product.ToString());
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Я ФУТБОЛЬНЫЙ МЯЧИК");
+            }
+
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            Lable_text.Items.Clear();
+            try
+            {
+                foreach (Product product in strings)
+                {
+                    if (Convert.ToInt32(product.Shelf_Life) > Convert.ToInt32(Name_shelf_life.Text))
+                    {
+                        Lable_text.Items.Add(product.ToString());
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("Я ФУТБОЛЬНЫЙ МЯЧИК");
+            }
+        }
+    }
+}
---
+++ WpfApp1/Product.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public class Product
+    {
+        public string a;
+        public string Name_Product { get; set; }
+        public string Name_Dic { get; set; }
+        public int Price_Product { get; set; }
+        public string Shelf_Life { get; set; }
+        public int Count_Product { get; set; }
+
+        public Product(string name, string dic, int price, string shelf, int count_p)
+        {
+            Name_Product = name;
+            Name_Dic = dic;
+            Price_Product = price;
+            Shelf_Life = shelf;
+            Count_Product = count_p;
+        }
+        public override string ToString()
+        {
+            return $"{Name_Product}, {Name_Dic}, {Price_Product}, {Shelf_Life}, {Count_Product}";
+        }
+    }
+}
