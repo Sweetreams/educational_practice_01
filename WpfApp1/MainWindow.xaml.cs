bab28e55cff6e9b03cb18d6392009b29d60f24c2
Sweetreams <pashasurov12345@gmail.com>
2023-12-24 23:47:37 +0700

    Я В РОТ БЕАЛ

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -24,7 +24,6 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         public List<Product> strings = new List<Product>();
-        public List<String> products = new List<String>();
 
         public MainWindow()
         {
@@ -36,7 +35,8 @@ namespace WpfApp1
             try
             {
                 Product product = new Product(Name_Product.Text, Name_Dic.Text, Convert.ToInt32(Price.Text), Convert.ToDateTime(Shelf_Life_Pro.Text), Convert.ToInt32(Count_Product.Text));
-                this.ProductList.Items.Add(product);
+                strings.Add(product);
+                ProductList.Items.Add(product);
             }
             catch (Exception ex)
             {
@@ -44,79 +44,41 @@ namespace WpfApp1
             }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            
-        }
-
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            this.ProductList.Items.Clear();
             if (Search_Name.IsSelected == true)
             {
+                ProductList.Items.Clear();
                 foreach (Product product in strings)
                 {
                     if (product.Name_Product == Name_search.Text)
                     {
-                        this.ProductList.Items.Add(product);
+
+                        ProductList.Items.Add(product);
                     }
                 }
             }
-
-            //Lable_text.Items.Clear();
-            //try
-            //{
-            //    foreach(Product product in strings)
-            //    {
-            //        if(product.Name_Product == Name_search.Text)
-            //        {
-            //            Lable_text.Items.Add(product.ToString());
-            //        }
-            //    }
-            //}
-            //catch
-            //{
-            //    MessageBox.Show("Я ФУТБОЛЬНЫЙ МЯЧИК");
-            //}
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            //Lable_text.Items.Clear();
-            //try
-            //{
-            //    foreach (Product product in strings)
-            //    {
-            //        if (product.Price_Product < Convert.ToInt32(Name_price_s.Text))
-            //        {
-            //            Lable_text.Items.Add(product.ToString());
-            //        }
-            //    }
-            //}
-            //catch
-            //{
-            //    MessageBox.Show("Я ФУТБОЛЬНЫЙ МЯЧИК");
-            //}
+            else if (Search_Price.IsSelected == true)
+            {
+                ProductList.Items.Clear();
+                foreach (Product product in strings)
+                {
+                    if (product.Price_Product == Convert.ToInt32(Name_search.Text))
+                    {
+                        ProductList.Items.Add(product);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Я хз");
+            }
 
         }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
+        // TODO: Сделать открытие csv-фалйа b и запись данных в product
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
         {
-            //Lable_text.Items.Clear();
-            //try
-            //{
-            //    foreach (Product product in strings)
-            //    {
-            //        if (Convert.ToInt32(product.Shelf_Life) > Convert.ToInt32(Name_shelf_life.Text))
-            //        {
-            //            Lable_text.Items.Add(product.ToString());
-            //        }
-            //    }
-            //}
-            //catch
-            //{
-            //    MessageBox.Show("Я ФУТБОЛЬНЫЙ МЯЧИК");
-            //}
+
         }
     }
 }
